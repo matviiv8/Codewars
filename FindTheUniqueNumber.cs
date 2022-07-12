078b8d33a586345269636bcb9ccc2cd23036f7ee
matviiv8 <73823120+matviiv8@users.noreply.github.com>
2022-07-12 19:18:43 +0300

    Update FindTheUniqueNumber.cs

--- FindTheUniqueNumber.cs
+++ FindTheUniqueNumber.cs
@@ -8,6 +8,7 @@ It’s guaranteed that array contains at least 3 numbers.
 The tests contain some very huge arrays, so think about performance.
 */
 
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
