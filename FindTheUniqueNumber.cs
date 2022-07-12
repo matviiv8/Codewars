277553bf4f0614592c23915da3f6ed8598230d3e
matviiv8 <73823120+matviiv8@users.noreply.github.com>
2022-07-12 19:14:09 +0300

    Create FindTheUniqueNumber.cs

---
+++ FindTheUniqueNumber.cs
@@ -0,0 +1,20 @@
+/*
+There is an array with some numbers. All numbers are equal except for one. Try to find it!
+
+findUniq([ 1, 1, 1, 2, 1, 1 ]) === 2
+findUniq([ 0, 0, 0.55, 0, 0 ]) === 0.55
+It’s guaranteed that array contains at least 3 numbers.
+
+The tests contain some very huge arrays, so think about performance.
+*/
+
+using System.Collections.Generic;
+using System.Linq;
+
+public class Kata
+{
+    public static int GetUnique(IEnumerable<int> numbers)
+    {
+        return numbers.GroupBy(x => x).Where(x => x.Count() == 1).Select(x => x.Key).First();
+    }
+}
