51fd2b12c735abe19e95300f29d5257d3fddc94b
matviiv8 <73823120+matviiv8@users.noreply.github.com>
2022-07-12 19:58:40 +0300

    Create YouCan'tCodeUnderPressure.cs

---
+++ 8 kyu/YouCan'tCodeUnderPressure.cs
@@ -0,0 +1,13 @@
+/*
+Code as fast as you can! You need to double the integer and return it.
+*/
+
+using System;
+
+public static class Kata
+{
+    public static int DoubleInteger(int n)
+    {
+        return 2 * n;
+    }
+}
