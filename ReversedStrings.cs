e0bba7af8c72e8af09bbad33f60ba730830b49e2
matviiv8 <73823120+matviiv8@users.noreply.github.com>
2022-07-12 19:19:29 +0300

    Create ReversedStrings.cs

---
+++ ReversedStrings.cs
@@ -0,0 +1,17 @@
+/*
+Complete the solution so that it reverses the string passed into it.
+
+'world'  =>  'dlrow'
+'word'   =>  'drow'
+*/
+
+using System;
+using System.Linq;
+
+public static class Kata
+{
+    public static string Solution(string str)
+    {
+        return new String(str.Reverse().ToArray());
+    }
+}
